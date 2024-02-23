5a4a62a979394f78d88acd7dc0c776fbc687d6c6
LukasDeco <ldeconantsesznak@gmail.com>
2024-02-23 15:10:50 -0600

    Feat: candy machine querying impl

--- src/pages/api/candymachine-update-owners.ts
+++ src/pages/api/candymachine-update-owners.ts
@@ -1,7 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { ShyftSdk, Network, CandyMachineProgram } from '@shyft-to/js';
+import { createClient } from "@supabase/supabase-js";
 
-const shyftClient = new ShyftSdk({ apiKey: process.env.NEXT_SHYFT_API_KEY ?? '', network: Network.Mainnet });
+const supabaseUrl = process.env.NEXT_SUPABASE_DB_URL ?? '';
+const supabaseKey = process.env.NEXT_SUPABASE_DB_KEY ?? '';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+const shyftClient = new ShyftSdk({ apiKey: process.env.NEXT_SHYFT_API_KEY ?? '', network: Network.Devnet });
 
 type ShyftArrayResultResponse = {
     success: boolean;
@@ -24,14 +29,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
         else if (network === 'devnet') shyftNetwork = Network.Devnet;
         else if (network === 'testnet') shyftNetwork = Network.Testnet;
         else throw new Error('WRONG_NETWORK');
+        var cmMints: string[];
 
-        var cm_mints: any[] = [];
-        var getMintsFromCandyMachine: string[];
+        const cm_version = (version === "v3") ? CandyMachineProgram.V3 : CandyMachineProgram.V2;
 
-        const cm_version = (version === "v3")?CandyMachineProgram.V3:CandyMachineProgram.V2;
-    
         try {
-            getMintsFromCandyMachine = await shyftClient.candyMachine.readMints({
+            cmMints = await shyftClient.candyMachine.readMints({
                 network: shyftNetwork,
                 address: cm_address,
                 version: cm_version,
@@ -43,17 +46,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
         // console.log("here2.1");
         // console.log(getMintsFromCandyMachine);
         // console.log("here3");
-        if (getMintsFromCandyMachine && getMintsFromCandyMachine.length > 0) {
-            for (let index = 0; index < getMintsFromCandyMachine.length; index++) {
-                const nft = getMintsFromCandyMachine[index];
-                cm_mints.push(nft);
-            }
-        } else throw new Error('NO_NFTS_IN_CM');
+
+        if (!cmMints || cmMints.length == 0) {
+            throw new Error('NO_NFTS_IN_CM');
+        }
+
+
+        // do random crap
+        const nfts = await getNftsInCollection(shyftNetwork, cm_address)
+        const dbUpdateSuccess = await pushNftDataToDatabase(cm_address, nfts, shyftNetwork);
+        if (!dbUpdateSuccess) {
+            throw new Error('FAILED_TO_UPDATE_DB');
+        }
 
         res.status(200).json({
             success: true,
             message: 'All Nfts in this CM',
-            result: cm_mints,
+            result: cmMints as any[],
         });
     } catch (error: any) {
         console.log(error);
@@ -65,34 +74,65 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     }
 }
 
-// async function pushNftDataToDatabase(reference_address: string, addresses_to_monitor: string[], network: Network):Promise<boolean> {
-//     try {
-//         if(addresses_to_monitor.length > 0)
-//         {
-//                 // var objectToBePushed = {};
+async function getNftsInCollection(shyftNetwork: Network, referenceAddress: string): Promise<string[]> {
+    let mints = [];
+    let currentPage = 0;
+    let totalPages = 0;
+    {
+        const getNftsInCollection = await shyftClient.nft.collection.getNfts({
+            network: shyftNetwork,
+            collectionAddress: referenceAddress,
+            size: 10,
+            page: currentPage
+        });
 
-//                 // const mintExists = await supabase
-//                 //     .from('monitor_mints')
-//                 //     .select()
-//                 //     .eq("mint_address",nft.mint);
 
-//                 // if(mintExists.count !== null)
-//                 //     objectToBePushed = mintExists.data[0];
+        if (totalPages === 0)
+            totalPages = getNftsInCollection.total_pages;
+        const nftReceived: any[] = getNftsInCollection.nfts;
+        for (let index = 0; index < nftReceived.length; index++) {
+            const nftElement: string = nftReceived[index].mint;
+            mints.push(nftElement);
+        }
 
-//                 // objectToBePushed ={ ...objectToBePushed, mint_address: nft.mint, current_holder: nft.owner};
 
-//                 //  //get from DB then push
-//                 // const insertToDb = await supabase.from('monitor_mints').upsert(objectToBePushed);
-//                 // if (insertToDb.error !== null)
-//                 //     throw new Error('INSERT_TO_DB_FAILED');
-//         }
-//         else
-//         {
-//             return false;
-//         }
-//         return true;
-//     } catch (error:any) {
-//         console.log(error);
-//         return false;
-//     }
-// }
+        currentPage++;
+    } while (currentPage < totalPages);
+
+    return mints;
+}
+
+async function pushNftDataToDatabase(referenceAddress: string, addresses_to_monitor: string[], network: Network): Promise<boolean> {
+    try {
+        if (addresses_to_monitor.length > 0) {
+            if (referenceAddress && addresses_to_monitor.length && network) {
+                const nftOwners = await shyftClient.nft.getOwners({ network: network, mints: addresses_to_monitor });
+                console.log(nftOwners);
+                if (nftOwners.length === 0)
+                    throw new Error('NO_NFT_DATA');
+
+
+                const allOwners: object[] = nftOwners;
+                // const allOwners:object[] = [];
+                for (var i = 0; i < allOwners.length; i++) {
+                    const eachOwner: any = allOwners[i];
+                    //fetch NFT metadata here
+                    const insertToDb = await supabase.from('monitor_mints').upsert({
+                        mint_address: eachOwner.nft_address,
+                        current_holder: eachOwner.owner
+                    });
+                    if (insertToDb.error !== null)
+                        throw new Error('INSERT_TO_DB_FAILED');
+                }
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+        return true;
+    } catch (error: any) {
+        console.log(error);
+        return false;
+    }
+}
